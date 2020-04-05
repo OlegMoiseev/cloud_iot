d8c1c3333ba7feca93133ec90a3e0103d39b5d7a
oleg <olegmoiseev2698@gmail.com>
2020-04-05 23:31:17 +0300

    Added timeout

--- static/js/app.js
+++ static/js/app.js
@@ -12,24 +12,35 @@ function getInfoAll() {
         var rowCount = table.rows.length;
         for (var i = tableHeaderRowCount; i < rowCount; i++) {
             table.deleteRow(tableHeaderRowCount);
-}
-
-
-        console.log("empted...");
+        }
         document.getElementById("trashDate").innerHTML = "Trash at " + Date().toString();
-
+        document.getElementById("totalTrashes").innerHTML = String(result.length);
+        let countFull = 0;
+        let sumFullnesses = 0;
         for (let i = 0; i < result.length; ++i)
         {
             let vars = result[i].split(", ");
             var row = table.insertRow(i+1);  // because we have header of the table
+            sumFullnesses += parseInt(vars[1]);
+            if (parseInt(vars[1]) > 50)
+            {
+                ++countFull;
+            }
             for (let j = 0; j < vars.length; ++j)
             {
                 row.insertCell(j).innerHTML = vars[j];
             }
         }
+
+        document.getElementById("filledTrashes").innerHTML = String(countFull);
+        document.getElementById("freeTrashes").innerHTML = String(result.length - countFull);
+        document.getElementById("averageLoading").innerHTML = String((sumFullnesses / result.length).toFixed(1)) + '%';
+
     };
     xhr.onerror = function () {
         console.log("Some error occurred - msg from onerror func");
     };
     xhr.send();
 }
+
+let timerReload = setInterval(getInfoAll, 1000);
