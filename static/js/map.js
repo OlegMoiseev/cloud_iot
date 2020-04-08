873ab9c7d8e5f189c3e9f41120c2023109ff273a
oleg <olegmoiseev2698@gmail.com>
2020-04-08 23:02:44 +0300

    Added Yandex.map

---
+++ static/js/map.js
@@ -0,0 +1,31 @@
+ymaps.ready(function(){
+        // Указывается идентификатор HTML-элемента.
+    var lat_center = 59.913743;
+    var long_center = 29.774067;
+
+    var trash_map = new ymaps.Map("trash_map", {
+        center: [lat_center, long_center],
+        zoom: 10
+    });
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', '/get_all', true);
+    xhr.onload = function () {
+        var text = xhr.responseText;
+        text = text.slice(2, -2);
+        var result = text.split("), (");
+
+        for (let i = 0; i < result.length; ++i)
+        {
+            let vars = result[i].split(", ");
+            var lat = parseFloat(vars[2]);
+            var long = parseFloat(vars[3]);
+            var trash_can = new ymaps.Placemark([lat, long]);
+            trash_map.geoObjects.add(trash_can);
+        }
+    };
+    xhr.onerror = function () {
+        console.log("Some error occurred - msg from onerror func");
+    };
+    xhr.send();
+});
--- test_server.py
+++ test_server.py
@@ -1,12 +1,12 @@
 import requests
 
-# data = {"latitude": 12.325567,
-#         "longitude": 43.271323}
-#
-# r = requests.post("http://0.0.0.0:5000/add", json=data)
-# print(r.text)
+data = {"latitude": 59.890493,
+        "longitude": 30.289959}
 
-data = {"id": 1,
-        "fullness": 26}
-r = requests.post("http://0.0.0.0:5000/update", json=data)
+r = requests.post("http://trash-system.herokuapp.com/add", json=data)
 print(r.text)
+
+# data = {"id": 1,
+#         "fullness": 26}
+# r = requests.post("http://0.0.0.0:5000/update", json=data)
+# print(r.text)
